45ed08b3141e96ccac47a47672f0035d566b9d18
gustavo araujo <gustavo@ambientalamazonia.com>
2024-09-24 19:40:16 -0500

    Adicionar arquivos de projeto.

---
+++ Project1/Aulia_01_Tarefa_02.cpp
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    string name;
+    int idade;
+
+    cout << "Qual é o seu nome?" << endl;
+    cin >> name;
+
+    cout << "Qual é a sua idade?" << endl;
+    cin >> idade;
+
+    cout << "Seu nome é: " << name << "Sua idade é: " << idade;
+
+    return 0;
+}
